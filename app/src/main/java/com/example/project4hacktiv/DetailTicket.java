dd534e88c0f2b8c243f095e1a44447bf5c7f70d1
xenrobi <xenrobi@gmail.com>
2022-02-22 22:07:12 +0700

    Detail_UI_UX_NEW

---
+++ app/src/main/java/com/example/project4hacktiv/DetailTicket.java
@@ -0,0 +1,62 @@
+package com.example.project4hacktiv;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailTicket extends AppCompatActivity {
+
+
+    TextView idUserT, idbookingT, noKursiT,namauserT,tanggalT, waktuAsalT, waktuTujuanT, hargaT, namaBusT, namaAsalT, namaTujaunT;
+    String idbooking,namauser,namaAsal, namaTujuan,harga,waktuAsal,idBus,waktuTujuan,namaBus,Tanggal,noKursi;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_ticket);
+
+        getPrevIntent();
+
+        setText(idbooking,idbookingT, R.id.bkid3);
+        setText(namaBus,namaBusT, R.id.namaBusTicket3);
+        setText(namauser,namauserT, R.id.namaUser);
+        setText(namaAsal,namaAsalT, R.id.namaAsalTV3);
+        setText(namaTujuan,namaTujaunT, R.id.namaTujuanTV3);
+        setText(waktuAsal,waktuAsalT, R.id.waktuAsal3);
+        setText(waktuTujuan,waktuTujuanT, R.id.waktuTujuanTV3);
+        setText(Tanggal,tanggalT, R.id.tanggalTV3);
+        setText(harga,hargaT, R.id.hargaTV3);
+        setText(noKursi,noKursiT, R.id.idkursiTV3);
+
+
+    }
+
+
+
+    private void setText(String intent, @NonNull TextView textView, @IdRes int intt){
+        textView = findViewById(intt);
+        textView.setText(intent);
+    }
+
+    private void getPrevIntent(){
+        Intent intent = getIntent();
+        idbooking = intent.getStringExtra("idbook");
+        namauser = intent.getStringExtra("namaInt");
+        namaAsal = intent.getStringExtra("namaAsalInt");
+        namaTujuan = intent.getStringExtra("namaTujuanInt");
+        Tanggal = intent.getStringExtra("tanggalInt");
+        harga = intent.getStringExtra("hargaInt");
+        waktuAsal = intent.getStringExtra("waktuAsalInt");
+        waktuTujuan = intent.getStringExtra("waktuTujuanInt");
+        namaBus = intent.getStringExtra("namaBusInt");
+        noKursi = intent.getStringExtra("kursiInt");
+
+    }
+
+
+}
